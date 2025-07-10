d133fec2cca32d07e5c239fa8f12f808b21cdd60
Aditya Thakur <adityathakur19200@gmail.com>
2025-07-10 17:20:18 +0530

    Page

--- components/theme/theme-provider.tsx
+++ components/theme/theme-provider.tsx
@@ -1,13 +1,29 @@
 "use client"
 
-import * as React from "react"
+import type { ReactElement } from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = React.useState(false)
+import { createContext, useContext, useEffect, useState } from "react"
 
-  React.useEffect(() => {
+type Theme = "dark" | "light" | "system"
+
+type ThemeProviderState = {
+  theme: Theme
+  setTheme: (theme: Theme) => void
+}
+
+const initialState: ThemeProviderState = {
+  theme: "system",
+  setTheme: () => null,
+}
+
+const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps): ReactElement {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
     setMounted(true)
   }, [])
 
@@ -16,4 +32,12 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   }
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext)
+
+  if (context === undefined) throw new Error("useTheme must be used within a ThemeProvider")
+
+  return context
 }
